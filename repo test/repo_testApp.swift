63faef40c30dd56fa17eb53a0a013c9b32ea954d
samilao101 <delacruz101@gmail.com>
2022-08-22 06:23:20 -0400

    Initial Commit

---
+++ repo test/repo_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  repo_testApp.swift
+//  repo test
+//
+//  Created by Sam Santos on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct repo_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
